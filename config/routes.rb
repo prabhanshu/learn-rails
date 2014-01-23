6cfc2a2393b351608cecaa405c869d89bc60ea00
Prabhanshu Pandit <prabhanshupandit.pu@gmail.com>
2014-01-23 21:26:05 +0530

    dynamic home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ LearnRails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root to: 'visitors#new'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
